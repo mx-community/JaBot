0e821d72a56cb87ea7e14e29549fc7aa4f8c728c
the-27 <blackoficial2025@gmail.com>
2025-10-08 11:51:55 -0500

    🎃 𝙎𝙝𝙖𝙙𝙤𝙬𝘾𝙤𝙧𝙚 🎋

--- plugins/downloads-Ytmp3Doc.js
+++ plugins/downloads-Ytmp3Doc.js
@@ -173,6 +173,33 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       console.log("⚠️ Error al procesar miniatura:", err)
     }
 
+    const Shadow_url = await (await fetch("https://files.catbox.moe/ipahdi.jpg")).buffer()
+
+    // Objeto tipo catálogo (productMessage)
+    const fkontak = {
+      key: {
+        fromMe: false,
+        participant: "0@s.whatsapp.net",
+        remoteJid: "status@broadcast"
+      },
+      message: {
+        productMessage: {
+          product: {
+            productImage: {
+              mimetype: "image/jpeg",
+              jpegThumbnail: Shadow_url
+            },
+            title: "💾 𝐃𝐄𝐒𝐂𝐀𝐑𝐆𝐀 𝐂𝐎𝐌𝐏𝐋𝐄𝐓𝐀 ⚡",
+            description: "",
+            currencyCode: "USD",
+            priceAmount1000: 100000,
+            retailerId: "descarga-premium"
+          },
+          businessOwnerJid: "51919199620@s.whatsapp.net"
+        }
+      }
+    }
+    
     await conn.sendMessage(m.chat, {
       document: { url: result.download },
       mimetype: "audio/mpeg",
@@ -190,7 +217,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
           renderLargerThumbnail: true
         }
       }
-    }, { quoted: m })
+    }, { quoted: fkontak })
 
   } catch (err) {
     console.error("[Error en ytmp3doc]", err)
