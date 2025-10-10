ef9d391a3bc0e426cbb3da5b5f780a7935bad311
the-27 <blackoficial2025@gmail.com>
2025-10-10 11:55:45 -0500

    🎃 𝙎𝙝𝙖𝙙𝙤𝙬𝘾𝙤𝙧𝙚 🎋

---
+++ plugins/downloader-sound.js
@@ -0,0 +1,48 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) return m.reply(`🎧 *Ingresa un enlace válido de SoundCloud.*`)
+
+  await m.react('🎶')
+
+  try {
+
+    const res = await fetch('https://api.siputzx.my.id/api/d/soundcloud', {
+      method: 'POST',
+      headers: {
+        'accept': '*/*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ url: text })
+    })
+
+    const json = await res.json()
+    if (!json.status) throw ' No se pudo obtener el audio.'
+
+    const { title, url, thumbnail, user } = json.data
+    let msg = `
+𝗜 𝗡 𝗜 𝗖 𝗜 𝗔 𝗡 𝗗 𝗢 • 𝗗 𝗘 𝗦 𝗖 𝗔 𝗥 𝗚 𝗔 
+> 📌 ${title}
+> 🏔️ ${user}`
+
+    await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: msg }, { quoted: m })
+
+    await conn.sendMessage(m.chat, {
+      document: { url },
+      mimetype: 'audio/mpeg',
+      fileName: `${title}.mp3`
+    }, { quoted: m })
+
+    await m.react('✅')
+
+  } catch (err) {
+    console.error(err)
+    await m.reply('⚠️ Error al descargar el audio. Asegúrate de que el enlace sea válido o inténtalo más tarde.')
+  }
+}
+
+handler.help = ['soundcloud2']
+handler.tags = ['downloader']
+handler.command = ['soundcloud2', 'scdl']
+
+export default handler
