1626055b5389666ea4ca47dceb877184638099a8
DV-ux-alt <lopezacaro09@gmail.com>
2025-10-21 18:10:20 -0500

    Actualizar downloads-play.js

--- plugins/downloads-play.js
+++ plugins/downloads-play.js
@@ -16,13 +16,13 @@ const vistas = formatViews(views)
 const info = `「✦」Descargando *<${title}>*\n\n> ❑ Canal » *${author.name}*\n> ♡ Vistas » *${vistas}*\n> ✧︎ Duración » *${timestamp}*\n> ☁︎ Publicado » *${ago}*\n> ➪ Link » ${url}`
 const thumb = (await conn.getFile(thumbnail)).data
 await conn.sendMessage(m.chat, { image: thumb, caption: info ...rcanal}, { quoted: m })
-if (['play', 'yta', 'ytmp3', 'playaudio'].includes(command)) {
+if (['play', 'mp3'].includes(command)) {
 const audio = await getAud(url)
 if (!audio?.url) throw '⚠ No se pudo obtener el audio.'
 //m.reply(`> ❀ *Audio procesado. Servidor:* \`${audio.api}\``)
 await conn.sendMessage(m.chat, { audio: { url: audio.url }, fileName: `${title}.mp3`, mimetype: 'audio/mpeg' }, { quoted: m })
 await m.react('✔️')
-} else if (['play2', 'ytv', 'ytmp4', 'mp4'].includes(command)) {
+} else if (['play2', 'mp4'].includes(command)) {
 const video = await getVid(url)
 if (!video?.url) throw '⚠ No se pudo obtener el video.'
 //m.reply(`> ❀ *Vídeo procesado. Servidor:* \`${video.api}\``)
@@ -33,7 +33,7 @@ await m.react('✖️')
 return conn.reply(m.chat, typeof e === 'string' ? e : '⚠︎ Se ha producido un problema.\n> Usa *' + usedPrefix + 'report* para informarlo.\n\n' + e.message, m)
 }}
 
-handler.command = handler.help = ['play', 'yta', 'ytmp3', 'play2', 'ytv', 'ytmp4', 'playaudio', 'mp4']
+handler.command = handler.help = ['play', 'play2', 'mp4', 'mp3']
 handler.tags = ['descargas']
 handler.group = true
 
