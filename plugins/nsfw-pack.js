46aff50df688ee1a40c915ad575c1dde7b4db4ad
the-27 <blackoficial2025@gmail.com>
2025-10-26 11:51:01 -0500

    🍃 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 --- 𝐒𝐡𝐚𝐝𝐨𝐰𝐂𝐨𝐫𝐞 🍬

--- plugins/nsfw-pack.js
+++ plugins/nsfw-pack.js
@@ -1,26 +1,25 @@
 import fetch from 'node-fetch'
 
 const handler = async (m, { conn }) => {
-  if (!db.data.chats[m.chat].nsfw && m.isGroup) {
-  return m.reply(`ê•¥ El contenido *NSFW* estÃ¡ desactivado en este grupo.\n\nUn *administrador* puede activarlo con el comando:\nÂ» *${usedPrefix}nsfw on*`)
-  }
   try {
     const res = await fetch('https://api.waifu.pics/nsfw/waifu')
     if (!res.ok) throw new Error('No se pudo obtener el pack, intenta de nuevo...')
 
     const json = await res.json()
-    if (!json.url) throw new Error('La API no devolvio una URL valida')
+    if (!json.url) throw new Error('La API no devolvi¨® una URL v¨¢lida')
 
-    await conn.sendFile(m.chat, json.url, 'pack.jpg', '\`ğŸ˜ Aqui tienes tu pack\`', m)
+    await conn.sendFile(m.chat, json.url, 'pack.jpg', '\`Aqu¨ª tienes tu pack\`', m)
   } catch (error) {
     console.error(error)
-    m.reply('*Ocurrio un error al obtener el pack, intenta mas tarde.*')
+    m.reply('? Ocurri¨® un error al obtener el pack, intenta m¨¢s tarde.')
   }
 }
 
-handler.command = ['pack']
+handler.command = ['pack2']
 handler.tags = ['nsfw']
-handler.help = ['pack']
+handler.help = ['pack2']
+handler.level = 10
+handler.register = true
 handler.premium = true
 
 export default handler
