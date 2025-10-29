ef8368b30db65186155f513c466f13019bf3be32
the-27 <blackoficial2025@gmail.com>
2025-10-28 19:59:20 -0500

    🍃 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 --- 𝐒𝐡𝐚𝐝𝐨𝐰𝐂𝐨𝐫𝐞 🍬

--- plugins/download-hentai.js
+++ plugins/download-hentai.js
@@ -47,8 +47,8 @@ let handler = async (m, { conn, text, args, setting }) => {
       let cap = `◜ Hentai - Search ◞\n`;
 
       results.slice(0, 15).forEach((res, index) => {
-        cap += `${i + 1}. 🌸 *Título:* ${res.titulo}
-🔗 *Link:* ${res.url}`;
+        cap += `${index + 1}. 🌸 *Título:* ${res.titulo}
+🔗 *Link:* ${res.url}\n`;
       });
       m.reply(cap)
       m.react("✔️");
