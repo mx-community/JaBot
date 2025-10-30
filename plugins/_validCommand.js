a5751d40ec44d7a8d6768b677cbca0672df866d0
MX-DIGITAL <emprendejosuev@gmail.com>
2025-10-30 18:29:38 -0300

    Update _validCommand.js

--- plugins/_validCommand.js
+++ plugins/_validCommand.js
@@ -32,7 +32,7 @@ const similares = allCommands
 .slice(0, 3)
 
 let sugerencias = similares.length
-? similares.map(s => `❒ *${s.cmd}* con un ${s.score}%`).join('\n') : '📍  No se han encontrado comandos relacionados.'
+? similares.map(s => `❒  *#${s.cmd}* con un ${s.score}% parecido.`).join('\n') : '📍  No se han encontrado comandos relacionados.'
 
 const texto = ` ✦  Hola usuario, el comando *"#${command}"* no existe.
 - Use el comando *#menu* para ver la lista completa.
