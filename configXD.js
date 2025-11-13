dba2ba4f122aeb7454a6cd34aaed40b919786565
MX-DIGITAL <emprendejosuev@gmail.com>
2025-11-13 04:23:20 -0300

    Update configXD.js

--- configXD.js
+++ configXD.js
@@ -22,13 +22,7 @@ global.prems = []
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.libreria = "Baileys Multi Device"
-global.xMage = "https://qu.ax/BAuJt.jpg"
-global.xMage2 = "https://qu.ax/BTUHt.jpg"
-global.xMage3 = "https://qu.ax/shFmH.jpg"
-global.xImagen = "https://qu.ax/MooSb.jpg"
-global.xImagen2 = "https://qu.ax/XPDQK.jpg"
-global.xImagen3 = "https://qu.ax/GbfQk.jpg"
-global.vs = "^1.8.2 • Latest"
+global.vs = "^2.3.4 • Latest"
 global.nameqr = "ᴋᴀɴᴇᴋɪ-ʙᴏᴛ.ᴍᴅ"
 global.sessions = "Sessions/Principal"
 global.jadi = "Sessions/SubBot"
@@ -39,20 +33,16 @@ global.kanekiAIJadibts = true
 global.botname = "MX : BOT"
 global.textbot = "Designed and created by Alan.Js / @mdmx_mktg"
 global.dev = "Alan.Js"
-global.author = "@mx-principal"
+global.author = "@mx-community"
 global.etiqueta = "@mx-support"
 global.currency = "Monedas"
+global.currency2 = "Puntos"
 global.banner = "https://qu.ax/XPDQK.jpg"
 //global.icono = "https://i.pinimg.com/originals/b3/67/d5/b367d513d861de468305c32c6cd22756.jpg"
 global.catalogo = fs.readFileSync('./lib/catalogo.jpg')
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.group = "https://whatsapp.com/channel/0029VbC34Nt42DchIWA0q11f"
-global.community = "https://whatsapp.com/channel/0029VbC34Nt42DchIWA0q11f"
-global.channel = "https://whatsapp.com/channel/0029VbC34Nt42DchIWA0q11f"
-global.github = "https://github.com/Shadow-nex/KanekiBot-V3"
-global.gmail = "shadowcore.xyz@gmail.com"
 global.ch = {
 ch1: "120363318353263389@newsletter"
 }
