2ba0789403d29dcafd195d0c135e1df11a5ff78b
MX-DIGITAL <emprendejosuev@gmail.com>
2025-11-15 00:34:37 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -164,4 +164,10 @@ export default handler
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 
- 
+function clockString(ms) {
+const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, '0')).join(':')
+}
+
