1d7c6d9c649653abf15c5a566b0bf21510e5d540
MX-DIGITAL <emprendejosuev@gmail.com>
2025-11-15 07:52:27 -0300

    Update d-apk.js

--- plugins/d-apk.js
+++ plugins/d-apk.js
@@ -1,4 +1,63 @@
-import { search, download } from 'aptoide-scraper'
+import fs from 'fs';
+import fetch from 'node-fetch';
+let apkSession = new Map();
+let handler = async (m, { conn, text, usedPrefix, command, args }) => {
+  if (command === 'apk' && text) {
+const reactionMessage = await conn.sendMessage(m.chat, { text: `Buscando resultados, espere un momento...` }, { quoted: m })
+try {
+const response = await fetch(`https://delirius-apiofc.vercel.app/download/apk?query=${encodeURIComponent(text)}`);
+const data = await response.json();
+if (!data.status || !data.data)
+  throw new Error("No se encontró la aplicación.");
+const app = data.data;
+apkSession.set(m.chat, { app });
+const thumb = Buffer.from(await (await fetch(`${app.image}`)).arrayBuffer())
+let description = `·─┄ · ✦ *Apk : Download* ✦ ·
+
+⊹ ✎ *Nombre:* ${app.name}
+⊹ ✎ *Paquete:* ${app.id}
+⊹ ✎ *Publicado:* ${app.publish}
+⊹ ✎ *Tamaño total:* ${app.size}
+⊹ ✎ *Descargas:* ${app.stats.downloads.toLocaleString()}
+⊹ ✎ *Estrellas:* ${app.stats.rating.average}
+
+📍  Se ha encontrado esta aplicación.
+- Responda a este mensaje con los siguientes comandos para descargar.
+
+*${usedPrefix}si* 
+*${usedPrefix}-y`;
+await conn.sendMessage(m.chat, { text: description, mentions: [m.sender], contextInfo: { externalAdReply: { 
+title: app.name, 
+body: app.publish, 
+thumbnail: thumb, 
+sourceUrl: null, 
+mediaType: 1, renderLargerThumbnail: true }}}, { quoted: m })
+return;
+ }
+
+if (args[0] === 'si' || args[0] === '-y') {
+let session = apkSession.get(m.chat);
+if (!session) {
+return conn.sendMessage(m.chat, { text: `📍  Debes realizar una busqueda primero para despues descargarlo con este comando.\n\n• *Busque usando el comando:*\n${usedPrefix + command}\n\n• Por ejemplo:\n*${usedPrefix + command}* WhatsApp.`}, { quoted: m });
+}
+let { app } = session;
+const downloadUrl = app.download;
+await conn.sendMessage(m.chat, { text: `Descargando el archivo, espere un momento...` }, { quoted: m });
+await conn.sendMessage(m.chat, { document: { url: downloadUrl }, mimetype: "application/vnd.android.package-archive", fileName: `${app.name}.apk`, caption: `❒ *Nombre:* ${app.name}\n❒ *Bot:* ${botname}` }, { quoted: m });
+return;
+  }
+
+
+  if (command === 'apk' && !text) {
+return conn.sendMessage(m.chat, { text: `📍  Ingrese el comando y escriba el nombre de la aplicacion que desea buscar.\n\n• Por ejemplo:\n*${usedPrefix + command}* WhatsApp` }, { quoted: m });
+}
+};
+
+handler.command = /^(apk|app)$/i;
+export default handler;
+                       
+
+/*import { search, download } from 'aptoide-scraper'
 
 var handler = async (m, { conn, usedPrefix, command, text }) => {
 if (!text) return conn.sendMessage(m.chat, { text: `Ingrese el comando y escriba el nombre de la app o apk para descargar.\n\n• Por ejemplo:\n*#${command}* WhatsApp` }, { quoted: m })
@@ -30,3 +89,4 @@ handler.command = ['apk', 'app']
 
 
 export default handler
+*/
