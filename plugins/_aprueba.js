6ebd63a05e4421b39a59465af4df647e01fc6aaf
MX-DIGITAL <emprendejosuev@gmail.com>
2025-11-15 00:22:18 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -1,20 +1,167 @@
+import axios from 'axios'
 import fetch from 'node-fetch'
+import { xpRange } from '../lib/levelling.js'
+import fs from 'fs'
+import PhoneNumber from 'awesome-phonenumber'
 
-let handler = async (m, { conn, command, text, usedPrefix, args }) => {
-let mentionedJid = await m.mentionedJid
-let userId = mentionedJid.length > 0 ? mentionedJid[0] : (m.quoted ? await m.quoted.sender : m.sender)
-let from = await (async () => global.db.data.users[m.sender].name || (async () => { try { const n = await conn.getName(m.sender); return typeof n === 'string' && n.trim() ? n : m.sender.split('@')[0] } catch { return m.sender.split('@')[0] } })())()
-let who = await (async () => global.db.data.users[userId].name || (async () => { try { const n = await conn.getName(userId); return typeof n === 'string' && n.trim() ? n : userId.split('@')[0] } catch { return userId.split('@')[0] } })())()
-let str, query
+let handler = async (m, { conn, usedPrefix, __dirname, participants }) => {
+try {
+const user = global.db.data.users[m.sender] || {}
+const name = await conn.getName(m.sender)
+const premium = user.premium ? '✓' : '✘'
+const limit = user.limit || 0
+const totalreg = Object.keys(global.db.data.users).length
+const groupUserCount = m.isGroup ? participants.length : '-'
+const groupsCount = Object.values(conn.chats).filter(v => v.id.endsWith('@g.us')).length
+const uptime = clockString(process.uptime() * 1000)
+const fecha = new Date(Date.now())
+const locale = 'es-AR'
+const dia = fecha.toLocaleDateString(locale, { weekday: 'long' })
+const fechaTxt = fecha.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
+const hora = fecha.toLocaleTimeString(locale, { hour: '2-digit', minute: '2-digit' })
+const totalCommands = Object.keys(global.plugins).length
+const userId = m.sender.split('@')[0]
+const phone = PhoneNumber('+' + userId)
+const pais = phone.getRegionCode() || 'Desconocido 🌐'
+const perfil = await conn.profilePictureUrl(conn.user.jid, 'image').catch(() => `${global.mMages}`)
+const thumb = Buffer.from(await (await fetch(`${global.mImagen}`)).arrayBuffer())
 
-if (args[0] === "enojado") {
+ await m.react('👋🏻')
+if (command === 'menu') {
+let menu = `${hora}, ${dia}, ${fechaTxt}
 
-} else if (args[0] === "alegre" {
+╭──────────────• · · · 
+│🜲 *Usuario:* @${name}
+│✦ *Premium:* ${premium}
+│ⴵ *Actividad:* ${uptime}
+│⚇ *Bot:* ${(conn.user.jid == global.conn.user.jid ? 'Principal' : 'PreBot')}
+│々 *Versión:* ${vs} 
+╰──────────────• · · · 
+${readMore}
+\`\`\`
+╭───[ ⛉ INFORMACIÓN ⛉ ]─•
+│#info     │
+│#ping     │
+│#status   │
+│#ds       │
+│#main     │
+╰──[ @mx-community ]─•
 
-} else {
 
- }
+╭───[ ⛉ DESCARGAS ⛉ ]─•
+│#github   │✎ link.
+│#apk      │✎ texto.
+│#facebook │✎ link.
+│#instagram│✎ link.
+│#play     │✎ texto, link.
+│#mediafire│✎ link.
+│#mega     │✎ link.
+│#threads  │✎ link.
+│#pinterest│✎ link.
+╰──[ @mx-community ]─•
+
+
+╭───[ ⛉ GRUPOS ⛉ ]─•
+│#add      │✎ número.
+│#kick     │✎ mention.
+│#delete   │✎ meply.
+│#promote  │✎ mention.
+│#demote   │✎ mention.
+│#warn     │✎ mention.
+│#unwarn   │✎ mention.
+│#g-img    │✎ reply.
+│#g-name   │✎ texto.
+│#g-desc   │✎ texto.
+│#link     │
+╰──[ @mx-community ]─•
+
+
+╭───[ ⛉ RPG GAME ⛉ ]─•
+│#cofre    │
+│#lb       │
+│#levelup  │
+│#minar    │
+│#work     │
+│#ruleta   │✎ query.
+│#slot     │✎ query.
+│#color    │✎ query.
+│#balance  │
+│#dep      │✎ query.
+│#ret      │✎ query.
+╰──[ @mx-community ]─•
+
+
+╭───[ ⛉ PROPIETARIO ⛉ ]─•
+│#update   │
+│#autoadmin│
+│#bot-name │✎ texto.
+│#bot-desc │✎ texto.
+│#bot-img  │✎ reply.
+│#bot-px   │✎ texto.
+│#reprefix │
+│#gfile    │✎ query.
+│#dfile    │✎ query.
+╰──[ @mx-community ]─•
+\`\`\`
+`
+return conn.sendMessage(m.chat, { text: menu.trim(), mentions: [m.sender], contextInfo: { externalAdReply: { title: botname, body: textbot, thumbnail: thumbBot, sourceUrl: null, mediaType: 1, renderLargerThumbnail: true }}}, { quoted: m })
+if (args[0] === '1' || args[0] === 'info') {
+let menu1 = `╭──────────────• · · · 
+│🜲 *Usuario:* @${name} *(Prem: ${premium})*
+│ⴵ *Actividad:* ${uptime} / ${(conn.user.jid == global.conn.user.jid ? 'Bot Principal.' : 'PreBot.')}
+╰──────────────• · · · 
+
+\`\`\`
+╭───[ ⛉ DESCARGAS ⛉ ]─•
+│#github   │✎ link.
+│#apk      │✎ texto.
+│#facebook │✎ link.
+│#instagram│✎ link.
+│#play     │✎ texto, link.
+│#mediafire│✎ link.
+│#mega     │✎ link.
+│#threads  │✎ link.
+│#pinterest│✎ link.
+╰──[ @mx-community ]─•
+\`\`\`
+`
+return conn.sendMessage(m.chat, { text: menu1.trim(), mentions: [m.sender], contextInfo: { externalAdReply: { title: botname, body: textbot, thumbnail: thumbBot, sourceUrl: null, mediaType: 1, renderLargerThumbnail: true }}}, { quoted: m })
+} else if (args[0] === '2' || args[0] === 'descargas') {
+let menu2 = `╭──────────────• · · · 
+│🜲 *Usuario:* @${name} *(Prem: ${premium})*
+│ⴵ *Actividad:* ${uptime} / ${(conn.user.jid == global.conn.user.jid ? 'Bot Principal.' : 'PreBot.')}
+╰──────────────• · · · 
+
+\`\`\`
+╭───[ ⛉ DESCARGAS ⛉ ]─•
+│#github   │✎ link.
+│#apk      │✎ texto.
+│#facebook │✎ link.
+│#instagram│✎ link.
+│#play     │✎ texto, link.
+│#mediafire│✎ link.
+│#mega     │✎ link.
+│#threads  │✎ link.
+│#pinterest│✎ link.
+╰──[ @mx-community ]─•
+\`\`\`
+`
+return conn.sendMessage(m.chat, { text: menu2.trim(), mentions: [m.sender], contextInfo: { externalAdReply: { title: botname, body: textbot, thumbnail: thumbBot, sourceUrl: null, mediaType: 1, renderLargerThumbnail: true }}}, { quoted: m })
 }
+}
+} catch (e) {
+console.error(e)
+await conn.sendMessage(m.chat, { text: `*[ 📍 ]*  ERROR_COMMAND = Command error, try again and if the error persists, report the command.` }, { quoted: m })
+}
+}
+handler.help = ['help  <category>', 'menu  <category>']
+handler.tags = ['menus']
+handler.command = ['testmenu', 'mtest']
 
-handler.command = ["rc", "r"]
-export default handler.
+
+export default handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+ 
