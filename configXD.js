1a4ab7e89fe8b0a25d9b93c8b1779f08bc51f940
MX-DIGITAL <emprendejosuev@gmail.com>
2025-11-16 07:51:54 -0300

    Update configXD.js

--- configXD.js
+++ configXD.js
@@ -32,7 +32,7 @@ global.kanekiAIJadibts = true
 
 global.botname = "MX : BOT"
 global.botgroup = "https://chat.whatsapp.com/H1SzR4nk4qLHeI9cxwMBsW?mode=wwt"
-goobal.botcanal = "https://whatsapp.com/channel/0029VbBPMLXHrDZnt0R2iF1d"
+global.botcanal = "https://whatsapp.com/channel/0029VbBPMLXHrDZnt0R2iF1d"
 global.botweb = "https://wa.me/5493873655135"
 global.botmenu = "https://qu.ax/MooSb.jpg"
 global.botimg = "https://qu.ax/BAuJt.jpg"
