b5f8444fa5b3cd0423577178f6b8fc0aefbdbac1
MX-DIGITAL <emprendejosuev@gmail.com>
2025-11-18 20:58:29 -0300

    Create cv-upload.js

---
+++ plugins/cv-upload.js
@@ -0,0 +1,30 @@
+import { createHash } from 'crypto'
+import fetch from 'node-fetch'
+import moment from 'moment-timezone'
+
+const handler = async (m, { conn, command, usedPrefix, text }) => {
+try {
+let uploadXd = `·─┄ · ✦ *Upload : Files* ✦ ·
+\t⧆ \`\`\`📍 Sube archivos soportados.\`\`\`
+
+\t\t⧡ *${usedPrefix}turl* (imagen y video)
+\t\t⧡ *${usedPrefix}catbox* (imagen y video)
+\t\t⧡ *${usedPrefix}ibb* (imagen)
+
+> ${textbot}`
+const thumb = Buffer.from(await (await fetch(`https://qu.ax/sHcff.jpg`)).arrayBuffer())
+await conn.sendMessage(m.chat, { text: uploadXd, mentions: [m.sender], contextInfo: { externalAdReply: { 
+title: "々  U P L O A D  :  F I L E  々", 
+body: null, 
+thumbnail: thumb, 
+sourceUrl: null, 
+mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
+} catch (error) {
+await conn.sendMessage(m.chat, { text: `*[ 📍 ]*  ERROR_COMMAND = ${error}` }, { quoted: m })
+}}
+
+
+handler.tags = ['tools']
+handler.command = ['upload']
+
+export default handler
