d2f797fd0c053f566cb28af724f3fc25157860e6
MX-DIGITAL <emprendejosuev@gmail.com>
2025-11-18 00:40:45 -0300

    Update inteligence-nanobanana.js

--- plugins/inteligence-nanobanana.js
+++ plugins/inteligence-nanobanana.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+/*import fs from 'fs';
 import { img2img } from '../lib/nanobanana.js';
 const handler = async (m, { conn, text, usedPrefix, command }) => {
 const q = m.quoted ? m.quoted : m;
@@ -22,3 +22,4 @@ handler.tags = ['inteligencias'];
 handler.command = ['nanob', 'nano'];
 export default handler;
  
+*/
