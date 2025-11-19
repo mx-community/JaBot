b3d9893753ed441130aadbbb11c07edc65418acb
MX-DIGITAL <emprendejosuev@gmail.com>
2025-11-18 22:28:31 -0300

    Update sticker-s.js

--- plugins/sticker-s.js
+++ plugins/sticker-s.js
@@ -15,20 +15,35 @@ let mime = (q.msg || q).mimetype || q.mediaType || ''
 let txt = args.join(' ')
 if (/webp|image|video/g.test(mime) && q.download) {
 if (/video/.test(mime) && (q.msg || q).seconds > 16)
-return conn.sendMessage(m.chat, { text: `📍  El video no debe de durar mas de 15 segundos.\n• Recortelo y vuelva a intentarlo.` }, { quoted: m })
+return conn.sendMessage(m.chat, { text: `📍  El video no debe durar mas de 15 segundos.\n\n\t\t＃ Recorta el vídeo y inténtalo de nuevo.` }, { quoted: m })
 let buffer = await q.download()
+await m.react('⏳')
 let marca = txt ? txt.split(/[\u2022|]/).map(part => part.trim()) : [texto1, texto2]
 stiker = await sticker(buffer, false, marca[0], marca[1])
 } else if (args[0] && isUrl(args[0])) {
 let buffer = await sticker(false, args[0], texto1, texto2)
 stiker = buffer
 } else {
-return conn.sendMessage(m.chat, { text: `Ingrese el comando y responda a una imagen o un video para crear un sticker.` }, { quoted: m })
+let disponible = `·─┄ · ✦ *Created : Stickers* ✦ ·
+\t𝇈 📍 \`\`\`Crea stickers sin limite.\`\`\`
+
+\t\t⧡ *${usedPrefix + command}* (imagen y video)
+\t\t⧡ *${usedPrefix}brat* (texto)
+\t\t⧡ *${usedPrefix}qc* (texto)
+\t\t⧡ *${usedPrefix}exif* (texto/texto2)
+\t\t⧡ *${usedPrefix}d-exif* (defauld)
+
+
+> ${textbot}
+`
+return conn.sendMessage(m.chat, { text: disponible }, { quoted: m })
 }} catch (e) {
-await conn.sendMessage(m.chat, { text: `*[ 📍 ]*  ERROR_COMMAND = Command error, try again and if the error persists, report the command.` }, { quoted: m })
+await await conn.sendMessage(m.chat, { text: `*[ 📍 ]*  ERROR_COMMAND = ${e.message}` }, { quoted: m })
+await m.react('📍')
 } finally {
 if (stiker) {
 conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+await m.react('✅')
 }}}
 
 handler.help = ['sticker']
@@ -40,3 +55,5 @@ export default handler
 const isUrl = (text) => {
 return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)(jpe?g|gif|png)/, 'gi'))
 }
+
+  
