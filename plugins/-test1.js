c6d5495ffd71393c941fa00a632020f3bd8d1dd0
MX-DIGITAL <emprendejosuev@gmail.com>
2025-11-19 17:42:41 -0300

    Update -test1.js

--- plugins/-test1.js
+++ plugins/-test1.js
@@ -1,19 +1,74 @@
+import { promises as fs } from 'fs';
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
+const charactersFilePath = './src/database/personajes.json';
+const haremFilePath = './src/database/reclamados.json';
 
-    let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
-     if (!teks) return conn.reply(m.chat, `📝 Que escribo? Ejemplo : *${usedPrefix + command}* Hola puercos`, m)
-      m.react('⏳')
-      let img = global.toruAPI('fgmods', '/api/maker/txt', { text: teks }, 'toruKeys')
-      conn.sendFile(m.chat, img, 'img.png', `✅ Es mejor de lo que escribes tú ✍🏻`, m)
-      m.react('✅')
+async function loadCharacters() {
+try {
+const data = await fs.readFile(charactersFilePath, 'utf-8');
+return JSON.parse(data);
+} catch (error) {
+throw new Error('📍  No se pudo cargar el archivo characters.json.');
+}
+}
 
-  }
-  handler.help = ['txt']
-  handler.tags = ['fun']
-  handler.command = ['txt']
-  
-  export default handler
+async function loadHarem() {
+try {
+const data = await fs.readFile(haremFilePath, 'utf-8');
+return JSON.parse(data);
+} catch (error) {
+return [];
+}
+}
+
+let handler = async (m, { conn, args }) => {
+if (args.length === 0) {
+await conn.reply(m.chat, `📍  Debe de ingresar el nombre del personaje para ver su informacion.\n\n• Por ejemplo:\n*#${command}* Takeda Harumi`, m);
+return;
+}
+
+const characterName = args.join(' ').toLowerCase().trim();
+
+try {
+const characters = await loadCharacters();
+const character = characters.find(c => c.name.toLowerCase() === characterName);
+
+if (!character) {
+await conn.reply(m.chat, `📍  El personaje ( *${characterName}* ) no existe o está mal escrito.\n- Revise en el comando *#torw* para verificar su existencia.`, m);
+return;
+}
+
+const harem = await loadHarem();
+const userEntry = harem.find(entry => entry.characterId === character.id);
+const statusMessage = userEntry 
+? `Reclamado por @${userEntry.userId.split('@')[0]} 📌` 
+: 'Libre para reclamar. ✅';
+
+const message = `〆  P E R S O N A J E
+\t𝇈 📍 \`\`\`Informacion del personaje.\`\`\`
+
+> ${statusMessage}
+
+\t\t⚶ *Nombre:* ${character.name}
+\t\t⚶ *Genero:* ${character.gender}
+\t\t⚶ *Valor:* ${character.value}
+\t\t⚶ *Rango:* ${character.source}
+
+💾 ID: *${character.id}*
 
 
+> ${textbot}`;
+
+await conn.reply(m.chat, message, m, { mentions: [userEntry ? userEntry.userId : null] });
+} catch (error) {
+await conn.reply(m.chat, `📍 ${error.message}`, m);
+}
+};
+
+handler.help = ['charinfo <nombre del personaje>', 'winfo <nombre del personaje>', 'waifuinfo <nombre del personaje>'];
+handler.tags = ['anime'];
+handler.command = ['charinfo', 'winfo', 'waifuinfo'];
+handler.group = true;
+
+export default handler;
 	
