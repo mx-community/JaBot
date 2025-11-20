f72dc3a6bf82f79d0c27691c47a2ef2e4a6fa342
MX-DIGITAL <emprendejosuev@gmail.com>
2025-11-20 18:29:40 -0300

    Update rpg-lb.js

--- plugins/rpg-lb.js
+++ plugins/rpg-lb.js
@@ -1,23 +1,23 @@
 import { xpRange } from '../lib/levelling.js'
 
-let handler = async (m, { conn, args }) => {
+let handler = async (m, { conn, args, command }) => {
 let users = Object.entries(global.db.data.users).map(([key, value]) => ({ ...value, jid: key }))
 let sorted = users.sort((a, b) => (b.exp || 0) - (a.exp || 0))
 const page = Math.max(1, Math.min(parseInt(args[0]) || 1, Math.ceil(sorted.length / 10)))
 const startIndex = (page - 1) * 10
 const endIndex = startIndex + 10
-let text = `·─┄ · ✦ *Top : Usuarios* ✦ ·\n\n`
+let text = `〆  T O P  :  U S E R S\n\n`
 const slice = sorted.slice(startIndex, endIndex)
 for (let i = 0; i < slice.length; i++) {
 const { jid, exp, level, coin } = slice[i]
 let name = await (async () => global.db.data.users[jid].name || (async () => { try { const n = await conn.getName(jid); return typeof n === 'string' && n.trim() ? n : jid.split('@')[0] } catch { return jid.split('@')[0] } })())()
-text += `🜲 *Usuario ${startIndex + i + 1}:* @${name}\n`
-text += `⛁ *${currency}:* ${coin.toLocaleString()}\n`
-text += `⛁ *${currency2}:* ${exp.toLocaleString()}\n`
-text += `ᗢ *LVL:* ${level}\n\n`
+text += `\t🜲 ${startIndex + i + 1} : @${name}\n`
+text += `\t⛁ ${currency} : ${coin.toLocaleString()}\n`
+text += `\t⛁ ${currency2} : ${exp.toLocaleString()}\n`
+text += `\t✦ LVL : ${level}\n\n`
 }
-text += `> Pagina *${page}* entre *${Math.ceil(sorted.length / 10)}*`
-if (page < Math.ceil(sorted.length / 10)) text += `\n- Ingrese el comando *#lb ${page + 1}* para ver la siguiente pagina.`
+text += `> 📍 Pagina *${page}* entre *${Math.ceil(sorted.length / 10)}*`
+if (page < Math.ceil(sorted.length / 10)) text += `\n- Ingrese el comando *(#${command} ${page + 1})* para ver la siguiente pagina.`
 await conn.reply(m.chat, text.trim(), m, { mentions: conn.parseMention(text) })
 }
 
