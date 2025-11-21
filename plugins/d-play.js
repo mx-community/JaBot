d3f3a53f21a6fc4b17c8c29e04b53608c28aec59
MX-DIGITAL <emprendejosuev@gmail.com>
2025-11-21 16:14:41 -0300

    Update d-play.js

--- plugins/d-play.js
+++ plugins/d-play.js
@@ -200,10 +200,10 @@ const d = Math.floor(seconds / (3600 * 24))
 const h = Math.floor((seconds % (3600 * 24)) / 3600)
 const m = Math.floor((seconds % 3600) / 60)
 const s = Math.floor(seconds % 60)
-const dDisplay = d > 0 ? d + (d == 1 ? ':' : ':') : ''
-const hDisplay = h > 0 ? h + (h == 1 ? ':' : ':') : ''
-const mDisplay = m > 0 ? m + (m == 1 ? ':' : ' :') : ''
-const sDisplay = s > 0 ? s + (s == 1 ? ' ' : ' ') : ''
+const dDisplay = d > 0 ? d + (d == 1 ? 'd:' : 'd:') : ''
+const hDisplay = h > 0 ? h + (h == 1 ? 'h:' : 'h:') : ''
+const mDisplay = m > 0 ? m + (m == 1 ? 'm:' : 'm:') : ''
+const sDisplay = s > 0 ? s + (s == 1 ? 's' : 's') : ''
 return dDisplay + hDisplay + mDisplay + sDisplay
 }
 
