53e59ea646ea3f961d14dfd6d7d17da432aa6043
MX-DIGITAL <emprendejosuev@gmail.com>
2025-11-21 01:28:02 -0300

    Create hr-pais.js

---
+++ plugins/hr-pais.js
@@ -0,0 +1,58 @@
+import fetch from 'node-fetch';
+
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+if (!text) return conn.sendMessage(m.chat, { text: `Ingrese el comando y el nombre del pais para ver su informacion.\n\n• Por ejemplo:\n*#${command}* Argentina` }, { quoted: m });
+try {
+await m.react("⏳");
+let api = `https://delirius-apiofc.vercel.app/tools/flaginfo?query=${text}`;
+let response = await fetch(api);
+let json = await response.json();
+let datas = json.data;
+let park = `〆  C O U N T R Y  :  D A T A
+
+\t𝇈 📍 \`\`\`Informacion del país.\`\`\`
+
+\t⧆ *Nombre* ╮ 
+> ${datas.officialName} *(${text})*
+
+\t⧆ *Organización* ╮ 
+> ${datas.memberOf}
+
+\t⧆ *Capital* ╮ 
+> ${datas.capitalCity}
+
+\t⧆ *Continente* ╮ 
+> ${datas.continent}
+
+\t⧆ *Población* ╮ 
+> ${datas.population}
+
+\t⧆ *Prefijo* ╮ 
+> ${datas.callingCode}
+
+\t⧆ *Moneda* ╮ 
+> ${datas.currency}
+
+\t\t📍 *Descripción:*
+\t» ${datas.description}
+
+
+> ${textbot}`;
+let img = datas.image;
+//const thumb = Buffer.from(await (await fetch(``)).arrayBuffer());
+/*conn.sendMessage(m.chat, { text: park, mentions: [m.sender], contextInfo: { externalAdReply: { 
+title: "々  C O U N T R Y  :  D A T A  々", 
+body: botname, 
+thumbnail: thumb, 
+sourceUrl: null, 
+mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m });*/
+
+conn.sendMessage(m.chat, { image: { url: img }, caption: park }, { quoted: fkontak });
+} catch (e) {
+await conn.sendMessage(m.chat, { text: `*[ 📍 ]*  ERROR_COMMAND = ${e}` }, { quoted: m });
+m.react('❌');
+  }
+};
+
+handler.command = ['pais', 'flag'];
+export default handler;
