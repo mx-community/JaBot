acff277d7b2216abfe20bc83b3ed44c86bdc2b5b
MX-DIGITAL <emprendejosuev@gmail.com>
2025-11-21 02:30:57 -0300

    Update -test2.js

--- plugins/-test2.js
+++ plugins/-test2.js
@@ -64,8 +64,8 @@ fetch(`https://api.neoxr.eu/api/youtube?url=${userVideoData.url}&type=audio&qual
 extract: (data) => ({data: data.data.url, isDirect: false})
 },
 {
-url: () => fetch(`${global.APIs.stellar.url}/dow/ytmp3?url=${userVideoData.url}&key=GataDios`).then((res) => res.json()),
-extract: (data) => ({data: data?.data?.dl, isDirect: false})
+url: () => fetch(`https://api-shadowxyz.vercel.app/download/ytmp3V2?url=${userVideoData.url}`).then((res) => res.json()),
+extract: (data) => ({data: data?.data?.download_url, isDirect: false})
 },
 {
 url: () => fetch(`https://api.siputzx.my.id/api/d/ytmp4?url=${userVideoData.url}`).then((res) => res.json()),
